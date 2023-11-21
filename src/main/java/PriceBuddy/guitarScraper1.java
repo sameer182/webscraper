51de4054d00e388103803e9d13488d0b24836999
Sameer Limbu <sameerlimbu31@gmail.com>
2023-11-21 16:20:19 +0000

    Added the loop function for the pages

--- src/main/java/PriceBuddy/guitarScraper1.java
+++ src/main/java/PriceBuddy/guitarScraper1.java
@@ -17,38 +17,42 @@ public class guitarScraper1 {
     }
 
     void scrape() throws Exception {
+        //Loop for the pages
+        for (int page = 1; page <= 37; page++) {
 
-        // Connect to the URL and retrieve the HTML document
-        Document doc = Jsoup.connect("https://www.guitar.co.uk/guitars/electric").get();
+            // Connect to the URL and retrieve the HTML document
+            Document doc = Jsoup.connect("https://www.guitar.co.uk/guitars/electric?p=" + page).get();
 
-        Elements mainContainer = doc.select("li.item");
+            Elements mainContainer = doc.select("li.item");
 
-        //Go through each product container
-        for (Element productContainer : mainContainer) {
+            //Go through each product container
+            for (Element productContainer : mainContainer) {
 
-            Elements productNameElement = productContainer.select(".product-name");
-            Elements desGuitar = productContainer.select(".product-description");
-            Elements priceGuitar = productContainer.select(".regular-price .price, .special-price .price");
+                Elements productNameElement = productContainer.select(".product-name");
+                Elements desGuitar = productContainer.select(".product-description");
+                Elements priceGuitar = productContainer.select(".regular-price .price, .special-price .price");
 
-           // Extract the brand name from the first word of the product name which is title.
-            for (Element product : productNameElement) {
-                String productName = product.select(".product-name").text();
-                String[] words = productName.split("\\s+");
-                if (words.length > 0) {
-                    String brand = words[0];
-                    String description = desGuitar.text();
+                // Extract the brand name from the first word of the product name which is title.
+                for (Element product : productNameElement) {
+                    String productName = product.select(".product-name").text();
+                    String[] words = productName.split("\\s+");
+                    if (words.length > 0) {
+                        String brand = words[0];
+                        String description = desGuitar.text();
 
-                    String priceString = priceGuitar.text().replaceAll("[^0-9]", "");
-                    int price = Integer.parseInt(priceString) / 100;
+                        String priceString = priceGuitar.text().replaceAll("[^0-9]", "");
+                        int price = Integer.parseInt(priceString) / 100;
 
-                    //Print the details for each product
-                    System.out.println("Name :" + productName + "\nBrand :" + brand + "\nDescription :" + description + "\nPrice :" + price);
-                    System.out.println("===============================================================================");
+                        //Print the details for each product
+                        System.out.println("Name :" + productName + "\nBrand :" + brand + "\nDescription :" + description + "\nPrice :" + price);
+                        System.out.println("===============================================================================");
 
+                    }
                 }
             }
         }
     }
+
           public static void main (String[]args){
 
             new guitarScraper1();
