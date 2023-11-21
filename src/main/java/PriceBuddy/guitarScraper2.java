25825b9d32627a087928d16d9af88e09334e6608
Sameer Limbu <sameerlimbu31@gmail.com>
2023-11-21 17:15:01 +0000

    Tested and Scraped the name and description successfully.

--- src/main/java/PriceBuddy/guitarScraper2.java
+++ src/main/java/PriceBuddy/guitarScraper2.java
@@ -20,8 +20,17 @@ public class guitarScraper2 {
      Document doc = Jsoup.connect("https://www.gak.co.uk/en/electric-guitars")
              .userAgent("Google Chrome - Computer Science Student - Webscraping Coursework Project").get();
 
-     Elements mainContainer = doc.select(".product-listing-container");
-        System.out.println(mainContainer);
+     Elements mainContainer = doc.select(".product-card");
+
+     for (Element productContainer : mainContainer) {
+
+         Elements guitarName = productContainer.select(".product-card .clamp.title");
+         Elements guitarDes = productContainer.select(".product-card .clamp.teaser");
+         System.out.println("Name :" + guitarName.text() + "\nDescription :" + guitarDes.text());
+         System.out.println("===========================================");
+
+     }
+
 
 
 }
