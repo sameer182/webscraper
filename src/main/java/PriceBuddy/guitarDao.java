026047b4b97b8d05877dc8cecaba61f5aa730ad7
Sameer Limbu <sameerlimbu31@gmail.com>
2023-12-17 00:45:45 +0000

    Added Data Access Object and update scraper1 file.

--- src/main/java/PriceBuddy/guitarDao.java
+++ src/main/java/PriceBuddy/guitarDao.java
@@ -6,91 +6,123 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.util.List;
+    public class guitarDao {
+        SessionFactory sessionFactory;
 
-public class guitarDao {
-    SessionFactory sessionFactory;
-
-    /**
-     * Demonstrates simple saving without merging
-     */
-    public guitar simpleSave(guitar guitar) {
-        //Get a new Session instance from the session factory and start transaction
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-
-        //Add Student, degree and university to database - will not be stored until we commit the transaction
-        session.saveOrUpdate(guitar);
-
-        //NOTE: THIS IS THE WRONG WAY TO SAVE DATA - IT WILL CREATE A LOT OF DUPLICATE ROWS!
-
-        //Commit transaction to save it to database
-        session.getTransaction().commit();
-
-        //Close the session and release database connection
-        session.close();
-        System.out.println("Guitar added to database with ID: " + guitar.getId());
-        return guitar;
-    }
-
-    public void init() {
-        try {
-            //Create a builder for the standard service registry
-            StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
-
-            //Load configuration from hibernate configuration file.
-            //Here we are using a configuration file that specifies Java annotations.
-            standardServiceRegistryBuilder.configure("hibernate.cfg.xml");
-
-            //Create the registry that will be used to build the session factory
-            StandardServiceRegistry registry = standardServiceRegistryBuilder.build();
-            try {
-                //Create the session factory - this is the goal of the init method.
-                sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        public guitar simpleSave(guitar guitar) {
+            try (Session session = sessionFactory.openSession()) {
+                session.beginTransaction();
+                session.saveOrUpdate(guitar);
+                session.getTransaction().commit();
+                System.out.println("Guitar added to the database with ID: " + guitar.getId());
+                return guitar;
             } catch (Exception e) {
-                    /* The registry would be destroyed by the SessionFactory,
-                        but we had trouble building the SessionFactory, so destroy it manually */
-                System.err.println("Session Factory build failed.");
+                System.err.println("Error saving guitar to the database.");
                 e.printStackTrace();
-                StandardServiceRegistryBuilder.destroy(registry);
+                return null;
             }
+        }
+
+        public comparison simpleSave(comparison comparison) {
+            try (Session session = sessionFactory.openSession()) {
+                session.beginTransaction();
+                session.saveOrUpdate(comparison);
+                session.getTransaction().commit();
+                System.out.println("Comparison added to the database with ID: " + comparison.getId());
+                return comparison;
+            } catch (Exception e) {
+                System.err.println("Error saving comparison to the database.");
+                e.printStackTrace();
+                return null;
+            }
+        }
+        public guitar saveAndMergeGuitar(guitar guitar, int price, String url) {
+            try (Session session = sessionFactory.openSession()) {
+                session.beginTransaction();
+
+//                // Check if a guitar with the same model already exists
+                guitar existingGuitar = getGuitarByModel(guitar.getModel());
+
+                if (existingGuitar == null) {
+                    // No existing guitar found, save the new guitar
+                    session.saveOrUpdate(guitar);
+                    System.out.println("New Guitar added to the database with ID: " + guitar.getId());
+                    existingGuitar = guitar; // Set existingGuitar to the newly added guitar
+                } else {
+                    // Existing guitar found, update the details
+                    existingGuitar.setName(guitar.getName());
+                    existingGuitar.setBrands(guitar.getBrands());
+                    existingGuitar.setDescription(guitar.getDescription());
+                    existingGuitar.setPic(guitar.getPic());
+//                    session.merge(existingGuitar);
+                    System.out.println("Existing Guitar merged in the database with ID: " + existingGuitar.getId());
+                }
+
+                // Save the comparison record
+                comparison gComparison = new comparison();
+                gComparison.setGuitar(existingGuitar);
+                gComparison.setPrice(price);
+                gComparison.setUrl(url);
+                session.save(gComparison);
+                System.out.println("Comparison added to the database with ID: " + gComparison.getId());
+
+                session.getTransaction().commit();
+                return existingGuitar;
+            } catch (Exception e) {
+                System.err.println("Error saving/merging guitar and comparison to the database.");
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        // Helper method to get a guitar by model
+        private guitar getGuitarByModel(String model) {
+            try (Session session = sessionFactory.openSession()) {
+                System.out.println("Searching for guitar with model: " + model);
+                return session.createQuery("FROM guitar WHERE LOWER(TRIM(model))= :model", guitar.class)
+                        .setParameter("model", model.trim())
+                        .uniqueResult();
+            } catch (Exception e) {
+                System.err.println("Error retrieving guitar by model from the database.");
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        public void init() {
+            try {
+                StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+                standardServiceRegistryBuilder.configure("hibernate.cfg.xml");
+                StandardServiceRegistry registry = standardServiceRegistryBuilder.build();
+
+                try {
+                    sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+                    // Enable SQL query logging
+                    sessionFactory.getProperties().put("hibernate.show_sql", "true");
+                } catch (Exception e) {
+                    System.err.println("Session Factory build failed.");
+                    e.printStackTrace();
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+
+                System.out.println("Session factory built.");
+            } catch (Throwable ex) {
+                System.err.println("SessionFactory creation failed." + ex);
+            }
+        }
 
 
-            //Output result
-            System.out.println("Session factory built.");
 
-        } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("SessionFactory creation failed." + ex);
+        public void setSessionFactory(SessionFactory sessionFactory) {
+            this.sessionFactory = sessionFactory;
+        }
+
+        public SessionFactory getSessionFactory() {
+            return sessionFactory;
         }
     }
 
-    /**
-     * Saves and merges data from student, degree and university classes
-     */
-    public void saveAndMerge(guitar guitar) throws Exception {
-        //Get a new Session instance from the session factory and start transaction
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
 
-        //First find or create university
-        String queryStr = "from guitar where name=" + guitar.getName()+ "";
-        List<guitar> guitarList = session.createQuery(queryStr).getResultList();
 
-        //University is in database
-        if (guitarList.size() == 1) {//Found a single University
-            //Update university location, if we want to
-            guitarList.get(0);
 
-        }
-        //No university with that name in database
-        else if (guitarList.size() == 0) {
-            session.saveOrUpdate(guitar.getName());
-        }
-        //Error
-        else {
-            throw new Exception("Multiple guitars with the same name");
-        }
-
-    }
-}
--- src/main/java/PriceBuddy/guitarScraper1.java
+++ src/main/java/PriceBuddy/guitarScraper1.java
@@ -5,57 +5,127 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 
-/** Example code to illustrate web scraping with JSoup */
-public class guitarScraper1 {
 
-    /**
-     * Constructor
-     */
-    guitarScraper1() {
-        try {
-            scrape();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+public class GuitarScraper1 {
+
+    private guitarDao dao;
+
+    public GuitarScraper1(guitarDao dao) {
+        this.dao = dao;
     }
 
-    void scrape() throws Exception {
+    void scrape() throws IOException {
+
         //Loop for the pages
-        for (int page = 1; page <=1; page++) {
+        for (int page = 1; page <= 20; page++) {
 
-            //Download HTML document from website
-            Document doc = Jsoup.connect("https://www.guitarguitar.co.uk/guitars/page-" + page + "/?Ordering=1&MinPrice=&MaxPrice=").get();
+            //Connect to the URL and retrieve the HTML document
+            Document doc = Jsoup.connect("https://richtonemusic.co.uk/electric-guitars/?page=" + page)
+                    .userAgent("Google Chrome - Computer Science Student - Webscraping Coursework Project").get();
 
-            //Get all the products on the page
-            Elements mainContainer = doc.select(".products");
-
+            Elements mainContainer = doc.select("li.product");
             //Go through each product
-            for (Element guitarSection : mainContainer.select(".product-inner")){
+            for (Element productContainer : mainContainer) {
                 //Get the product name
-                Elements guitarName = guitarSection.select(".qa-product-list-item-title");
-                //Get the guitar brand
-                Elements brandGuitar = guitarSection.select("strong");
+                Elements guitarNameElement = productContainer.select(".card-title");
 
-                //Get the guitar price
-                Elements priceGuitar = guitarSection.select("span.js-pounds");
-                //Remove all the non-numerical digit
-                String priceString = priceGuitar.text().replaceAll("[^0-9]", "");
-                int price = Integer.parseInt(priceString);
+                // Extract the brand name from the first word of the product name which is title.
+                for (Element product : guitarNameElement) {
+                    //Remove the unnecessary name after hyphen
+                    String name = product.select(".card-title").text().split("-")[0].trim();
+                    String[] words = name.split("\\s+");
+                    if (words.length > 0) {
+                        String brand = words[0];
 
-                //Get the product pic
-                Elements guitarPic = mainContainer.select(".js-picture-lazy img");
-                String pic = guitarPic.attr("data-src");
-                System.out.println(pic);
+                        //Get the product price
+                        Elements guitarPrice = productContainer.select(".price--withTax");
+                        //Remove all the non-numerical digit
+                        String priceString = guitarPrice.text().replaceAll("[^0-9]", "");
+                        int price = Integer.parseInt(priceString) / 100;
 
-//                System.out.println("Name: " + guitarName.text() + "\nBrand: " + brandGuitar.text() + "\nPrice: " + price);
-//                System.out.println("==========================");
+                        //Get the product description
+                        Elements guitarDes = productContainer.select(".summary, hide-grid");
+
+                        //Get the product picture
+                        Elements guitarPic = productContainer.select(".card-img-container img");
+                        String pic = guitarPic.attr("src");
+
+                        //Get the product URL
+                        Elements guitarUrl = productContainer.select(".card-figure a");
+                        String url = guitarUrl.attr("href");
+
+                        // Get the product model SKU
+                        String model = scrapeModel(url);
+                        System.out.println(model);
+
+
+                        // Create guitar class with data
+                        guitar g1 = new guitar();
+                        g1.setName(name);
+                        g1.setBrands(brand);
+                        g1.setModel(model);
+                        g1.setDescription(guitarDes.text());
+                        g1.setPic(pic);
+
+                        // Create comparison class with data
+                        comparison g1Comparison = new comparison();
+                        g1Comparison.setGuitar(g1);
+                        g1Comparison.setPrice(price);
+                        g1Comparison.setUrl(url);
+
+                        try {
+                            // Save data with checking for duplicates
+                            dao.saveAndMergeGuitar(g1, price, url);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
 
             }
+        }
+    }
+    //Scrape the guitar model from inside the product URL
+    String scrapeModel(String url) throws IOException {
+        Document productPage = Jsoup.connect(url).get();
+        Element modelElement = productPage.select("dd.productView-info-value").first();
+        // Remove dashes and convert to lowercase
+        if (modelElement != null) {
+            String modelName = modelElement.text().replace("SKU: ", "")
+                    .replaceAll("-", "").toLowerCase();
 
+            // Check if the first three characters are "gib" and trim them
+            if (modelName.length() >= 3 && modelName.substring(0, 3).equals("gib")) {
+                modelName = modelName.substring(3);
+            }
+            // Check if the first four characters are "fend" and trim them
+            if (modelName.length() >= 4 && modelName.substring(0, 4).equals("fend")) {
+                modelName = modelName.substring(4);
+            }
+            if (modelName.length() >= 3 && modelName.substring(0, 3).equals("epi")) {
+                modelName = modelName.substring(3);
+            }
+            if (modelName.length() >= 6 && modelName.substring(0, 6).equals("squier")) {
+                modelName = modelName.substring(6);
+            }
+            if (modelName.length() >= 4 && modelName.substring(0, 4).equals("jack")) {
+                modelName = modelName.substring(4);
+            }
+            if (modelName.length() >= 7 && modelName.substring(0, 7).equals("gretsch")) {
+                modelName = modelName.substring(7);
+            }
+            if (modelName.length() >= 4 && modelName.substring(0, 4).equals("char")) {
+                modelName = modelName.substring(4);
+            }
+
+
+            return modelName;
+        } else {
+            return "";
         }
     }
 
 }
-
 
